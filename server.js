2e530c2cf906346eb12aadaaf147c5e1ec1f8944
Petri Vesanen <petri.vesanen@kauppalehti.fi>
2016-12-22 11:33:19 +0200

    ATMEDIA-309: Use fixed version of PhantomJS.

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
+var consoleTEN = require('console-ten');
+consoleTEN.init(console, consoleTEN.LEVELS.INFO, function(levelName) {
+    return "" + (new Date()).toString() + " " + levelName + " ";
+});
+
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 5,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
 });
 
 
 // server.use(prerender.basicAuth());
-// server.use(prerender.whitelist());
+server.use(prerender.whitelist());
 server.use(prerender.blacklist());
 // server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
-// server.use(prerender.s3HtmlCache());
+server.use(prerender.s3HtmlCache());
 
 server.start();
